845614553be8bc7c29f19e5b564fd9202db4a9d9
itsemanuela <notemanu152@gmail.com>
2026-05-11 14:03:39 +0200

    es 1-4

--- typescript/index.js
+++ typescript/index.js
@@ -1,30 +1,20 @@
+"use strict";
 //1) Quali sono i tipi primitivi principali in TypeScript?
-
-
 //I dati primitivi in ts sono simili a quelli in js: abbiamo booleani, numeri, stringhe, undefined, null, ma ts introduce anche la novità ANY che non si utilizza in quanto spegne il controllo del file. Altra novità è l'operatore di assegnazione => :
-
-
 //2) Crea tre variabili tipizzate: una stringa con il tuo nome, un numero con la tua età, e un booleano che indica se stai studiando TypeScript.
-
-const nome: string="Emanuela";
-const età:number=27;
-const studiaTS: boolean=true;
-
-
-
+const nome = "Emanuela";
+const età = 27;
+const studiaTS = true;
 //3) Tipizza il parametro della seguente funzione:
 //const greet = (name) => { return "Ciao " + name }
-
-const greet=(name:string )=>{
-    return "Ciao" + " " + name;}
-
-    console.log(greet("Emanuela"));
-
-    //4) Specifica il tipo di ritorno della seguente funzione:
-    //const sum = (a: number, b: number) => { return a + b }
-
-    const sum=(a:number, b:number): number=> {
-        return a + b
-    }; console.log(sum(4, 5))  
- 
+const greet = (name) => {
+    return "Ciao" + " " + name;
+};
+console.log(greet("Emanuela"));
+//4) Specifica il tipo di ritorno della seguente funzione:
+//const sum = (a: number, b: number) => { return a + b }
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(4, 5));
 //:number mi speicifica che il valore di ritorno sarà un numero
